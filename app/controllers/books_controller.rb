5aaa915fa6dffef8c72e1a8a75e1f5eb03ef36c6
yuto-2222 <110211669+yuto-2222@users.noreply.github.com>
2023-09-11 08:38:38 +0000

    [update]bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
     @book.user_id = current_user.id
     @user = current_user
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
       render :index
